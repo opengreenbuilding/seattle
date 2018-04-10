0aba8bb2f488720213c4df24ecba1c5c6924645e
Greg Sugiyama <gjsugiyama@gmail.com>
2018-04-10 16:46:01 -0700

    waterfall.js adds fake eui, energy star and ghg values

--- scripts/waterfall.js
+++ scripts/waterfall.js
@@ -4,12 +4,28 @@ const json2csv = require("json2csv");
 const moment = require("moment")
 const BUILDINGS = require("./test.json");
 
+function randomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
 function initialRead(cb) {
-    console.log("Reading Data")
+    console.log("Reading data...")
     const buildingJSON = BUILDINGS.map(building => {
         return building
     })
-    cb(null, buildingJSON)
+    cb(null, buildingJSON);
+}
+
+function generateBuildingObject(buildingJSON, cb) {
+    console.log("Compiling building object...")
+    let dataset = buildingJSON.map(building => {
+      return {...building,
+        EUI: randomNumber(1, 100),
+        ENERGY_STAR_SCORE: randomNumber(1, 100),
+        GREENHOUSE_GAS_EMISSIONS: randomNumber(1, 100)
+       }
+    })
+    cb(null, dataset);
 }
 
 function CSVify(dataset, cb) {
@@ -17,7 +33,7 @@ function CSVify(dataset, cb) {
     const fields = _.keys(dataset[0]); 
     const csv = json2csv({ data: dataset, fields: fields },
         function (err, csv) {
-            console.log("CSV has been created");
+            console.log("CSV has been reated...");
             cb(err, csv);
         }
     )
@@ -25,14 +41,15 @@ function CSVify(dataset, cb) {
 
 async.waterfall([
     initialRead,
+    generateBuildingObject,
     CSVify
 ], 
     function(err, result) {
         if (err) {
             throw err;
         }
-        console.log('finished report pipeline');
-        console.log('writing report to stdout');
+        console.log("finished report pipeline");
+        console.log("writing report to stdout");
         process.stdout.write("# Generated at " + moment.utc().toISOString());
         process.stdout.write("\n");
         process.stdout.write(result);

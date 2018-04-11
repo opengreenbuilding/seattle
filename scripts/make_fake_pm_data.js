28bdb886961ba846eefe9257133c26b31865fac6
Greg Sugiyama <gjsugiyama@gmail.com>
2018-04-11 16:51:45 -0700

    make_fake_pm_data.js generates normalized data (eui, energy star and greenhouse gas) between -1 and +1 standard deviations away from the mean.

--- scripts/make_fake_pm_data.js
+++ scripts/make_fake_pm_data.js
@@ -10,6 +10,21 @@ function randomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
+function randomNumberOfStandardDeviations(min, max) {
+    return ((Math.random() * (max - min + 1) + min))
+}
+
+
+function standardDeviation(mean, dataArray, numberOfEntries) {
+    let sumDistanceFromMean = 0 
+    dataArray.map(data => {
+        sumDistanceFromMean += Math.pow((data - mean), 2);
+    })
+
+    let stdDev = Math.round(Math.sqrt((sumDistanceFromMean / numberOfEntries)))
+    return stdDev;
+}
+
 function initialRead(cb) {
     console.log("Reading data...")
     const buildingJSON = BUILDINGS.map(building => {
@@ -30,10 +45,49 @@ function generateBuildingObject(buildingJSON, cb) {
     cb(null, dataset);
 }
 
-function CSVify(dataset, cb) {
+function normalizeBuildingData(dataset, cb) {
+    let sumEUI = 0;
+    let sumEnergyStar = 0;
+    let sumGreenhouseGas = 0;
+
+    let dataEUI = [];
+    let dataEnergyStar = [];
+    let dataGreenhouseGas = [];
+
+    dataset.map(building => {
+        sumEUI += building.EUI;
+        sumEnergyStar += building.ENERGY_STAR_SCORE;
+        sumGreenhouseGas += building.GREENHOUSE_GAS_EMISSIONS;
+
+        dataEUI.push(building.EUI);
+        dataEnergyStar.push(building.ENERGY_STAR_SCORE);
+        dataGreenhouseGas.push(building.GREENHOUSE_GAS_EMISSIONS);
+    })
+
+    let meanEUI = Math.round((sumEUI / dataset.length))
+    let meanEnergyStar = Math.round((sumEnergyStar / dataset.length))
+    let meanGreenhouseGas = Math.round((sumGreenhouseGas / dataset.length))
+
+    let stdDevEUI = standardDeviation(meanEUI, dataEUI, dataset.length)
+    let stdDevEnergyStar = standardDeviation(meanEnergyStar, dataEnergyStar, dataset.length)
+    let stdDevGreenhouseGas = standardDeviation(meanGreenhouseGas, dataGreenhouseGas, dataset.length)
+
+    let normalizedBuildingsObj = dataset.map(building => {
+        return {
+            ...building,
+            EUI: (Math.sign(randomNumberOfStandardDeviations(-1, 1)) === -1) ? (Math.round(meanEUI + (randomNumberOfStandardDeviations(-1, 2) * stdDevEUI))) : (Math.round((randomNumberOfStandardDeviations(-1, 1) * stdDevEUI) + meanEUI)),
+            ENERGY_STAR_SCORE: (Math.sign(randomNumberOfStandardDeviations(-1, 1)) === -1) ? (Math.round(meanEUI + (randomNumberOfStandardDeviations(-1, 2) * stdDevEUI))) : (Math.round((randomNumberOfStandardDeviations(-1, 1) * stdDevEUI) + meanEUI)),
+            GREENHOUSE_GAS_EMISSIONS: (Math.sign(randomNumberOfStandardDeviations(-1, 1)) === -1) ? (Math.round(meanEUI + (randomNumberOfStandardDeviations(-1, 2) * stdDevEUI))) : (Math.round((randomNumberOfStandardDeviations(-1, 1) * stdDevEUI) + meanEUI))
+        }
+    })
+    
+    cb(null, normalizedBuildingsObj)
+}
+
+function CSVify(normalizedBuildingsObj, cb) {
     console.log("Generating CSV")
-    const fields = _.keys(dataset[0]); 
-    const csv = json2csv({ data: dataset, fields: fields },
+    const fields = _.keys(normalizedBuildingsObj[0]); 
+    const csv = json2csv({ data: normalizedBuildingsObj, fields: fields },
         function (err, csv) {
             console.log("CSV has been reated...");
             cb(err, csv);
@@ -44,6 +98,7 @@ function CSVify(dataset, cb) {
 async.waterfall([
     initialRead,
     generateBuildingObject,
+    normalizeBuildingData,
     CSVify
 ], 
     function(err, result) {

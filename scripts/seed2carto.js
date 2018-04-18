269fa7434a83cc4e43d97e788c50eea81d46c943
Greg Sugiyama <gjsugiyama@gmail.com>
2018-04-18 16:02:56 -0700

    Adds seed2carto script. Parses SEED benchmarking data and Inserts into Carto
    adds springfield data to carto for testing. All filters work on fake data

--- scripts/seed2carto.js
+++ scripts/seed2carto.js
@@ -2,61 +2,43 @@
 
 const request = require("request");
 const config = require("./seed2carto_secrets.js");
-
-function randomNumber(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
-
-function randomEui(max, min) {
-  let eui = Math.random() * (max - min);
-  return Math.floor(eui);
-}
-
-function randomGeom(max, min, decimals) {
-  let randomNum = Math.random() * (max - min) + min;
-  let power = Math.pow(10, decimals);
-  return Math.floor(randomNum * power) / power;
-}
-
 const today = new Date();
-const date =
-  today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
-const time =
-  today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+const date = today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
+const time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
 const dateTime = date + " " + time;
 
-request({ url: config.propertiesUrl }, (error, response, body) => {
+request({ url: config.seedPropertiesUrl }, (error, response, body) => {
   console.log(`Requesting Database Entries for ${config.seedUsername}`);
   if (error) {
-    console.log(error);
+    console.log("Error: ", error);
     return error;
   }
   let parsedBody = JSON.parse(body);
-  let seedBuildings = parsedBody.results;
-  let cartoBuildings = seedBuildings.map(building => {
+  let seedBuildings = parsedBody.properties.map(building => {
     return {
-      owner: building.owner,
-      address: building.address_line_1,
-      reported_gross_floor_area: building.gross_floor_area,
-      yearbuilt: building.year_built,
-      postal_code: building.postal_code,
-      id: building.id,
-      property_type: building.property_type,
-      ghg_intensity: randomNumber(10, 1), // THIS WILL NEED TO COME FROM REAL DATA AT SOME POINT.
-      ghg_emissions: randomNumber(500, 1), // THIS WILL NEED TO COME FROM REAL DATA AT SOME POINT.
-      property_name: "BUILDING NAME", // THIS WILL NEED TO COME FROM REAL DATA AT SOME POINT.
-      province: config.province, //THIS WILL NEED TO COME FROM REAL DATA AT SOME POINT.
-      reporting_year: 2015, //THIS WILL NEED TO COME FROM REAL DATA AT SOME POINT.
-      city: config.city, //THIS WILL NEED TO COME FROM REAL DATA AT SOME POINT.
-      energy_star_score: randomNumber(100, 1), //THIS WILL NEED TO COME FROM REAL DATA AT SOME POINT.
-      site_eui: randomNumber(100, 1), //THIS WILL NEED TO COME FROM REAL DATA AT SOME POINT.
-      longitude: randomGeom(123.16, 123.1, 6) * -1,
-      latitude: randomGeom(49.19, 49.15, 6)
-    };
+      latitude: Number(building.state.extra_data.latitude),
+      longitude: Number(building.state.extra_data.longitude),
+      city: building.state.city,
+      year: 2016,
+      postal_code: building.state.extra_data.PROPERTY_POSTAL_CODE,
+      reported_address: building.state.extra_data.reported_address,
+      property_type: building.state.extra_data.PRIMARY_USE,
+      property_name: building.state.extra_data.NOTES,
+      numunits: Number(building.state.extra_data.numunits),
+      numfloors: Number(building.state.extra_data.numfloors),
+      numbuildings: Number(building.state.extra_data.numbuildings),
+      yearbuilt: Number(building.state.extra_data.yearbuilt),
+      reported_gross_floor_area: Number(building.state.extra_data.reported_gross_floor_area),
+      site_eui: Number(building.state.site_eui),
+      source_eui: Number(building.state.source_eui),
+      energy_star_score: Number(building.state.extra_data.energy_star_score),
+      total_ghg_emissions: Number(building.state.extra_data.total_ghg_emissions),
+      total_ghg_emissions_intensity: Number(building.state.extra_data.total_ghg_emissions_intensity)
+    }
   });
   request.put(
-    `${config.cartoUrl}/sql?q=UPDATE ${config.cartoDataSet} SET deleted_at=
-    '${dateTime}' WHERE deleted_at IS NULL &api_key=20e0f2883fe8ea263d3b54cfa7758fb859961199`,
+    `${config.cartoQueryUrl} UPDATE ${config.cartoDataSet} SET deleted_at=
+    '${dateTime}' WHERE deleted_at IS NULL &api_key=${config.cartoApi}`,
     function(err, res, body) {
       console.log(`Writing Entries...`);
       if (err) {
@@ -64,64 +46,72 @@ request({ url: config.propertiesUrl }, (error, response, body) => {
         return err;
       }
       if (res.statusCode === 200) {
-        cartoBuildings.map(building => {
+        seedBuildings.map(building => {
           let reported_gross_floor_area = building.reported_gross_floor_area;
           let energy_star_score = building.energy_star_score;
           let yearbuilt = building.yearbuilt;
+          let source_eui = building.source_eui;
           let postal_code = building.postal_code;
-          let id = building.id;
-          let province = building.province;
           let city = building.city;
           let site_eui = building.site_eui;
-          let lat = building.latitude;
-          let long = building.longitude;
-          let building_owner = building.owner;
-          let reported_address = building.address;
-          let year = building.reporting_year;
+          let latitude = building.latitude;
+          let longitude = building.longitude;
+          let reported_address = building.reported_address;
+          let year = building.year;
+          let numunits = building.numunits;
+          let numfloors = building.numfloors;
+          let numbuildings = building.numbuildings;
           let property_name = building.property_name;
           let property_type = building.property_type;
-          let total_ghg_emissions_intensity = building.ghg_intensity;
-          let total_ghg_emissions = building.ghg_emissions;
-          console.log(property_type);
-          request.post(
-            `${cartoUrl}/sql?q=INSERT INTO ${cartoDataSet} (
-                    the_geom,
-                    reported_address,
-                    year,
-                    reported_gross_floor_area,
-                    yearbuilt,
-                    postal_code,
-                    id,
-                    province,
-                    city,
-                    site_eui,
-                    energy_star_score,
-                    property_name,
-                    property_type,
-                    total_ghg_emissions_intensity,
-                    total_ghg_emissions
-                    ) VALUES (
-                    ST_SetSRID(ST_Point(${long},${lat}),4326),
-                    '${reported_address}',
-                    ${year},
-                    ${reported_gross_floor_area},
-                    ${yearbuilt}, '${postal_code}',
-                    ${id},
-                    '${province}',
-                    '${city}',
-                    ${site_eui},
-                    ${energy_star_score},
-                    '${property_name}',
-                    '${property_type}',
-                    ${total_ghg_emissions_intensity},
-                    ${total_ghg_emissions}
-                    )&api_key=20e0f2883fe8ea263d3b54cfa7758fb859961199`,
-            function(err, res, body) {
+          let total_ghg_emissions_intensity = building.total_ghg_emissions_intensity;
+          let total_ghg_emissions = building.total_ghg_emissions;
+          let insertUrl = config.cartoQueryUrl + "INSERT INTO " + config.cartoDataSet + " ("
+                   + "the_geom, "
+                   + "reported_gross_floor_area, "
+                   + "energy_star_score, "
+                   + "yearbuilt, "
+                   + "postal_code, "
+                   + "city, "
+                   + "site_eui, "
+                   + "source_eui, "
+                   + "latitude, "
+                   + "longitude, "
+                   + "reported_address, "
+                   + "year, "
+                   + "property_name, "
+                   + "property_type, "
+                   + "total_ghg_emissions_intensity, "
+                   + "total_ghg_emissions, "
+                   + "numunits, "
+                   + "numfloors, "
+                   + "numbuildings" +
+                    ") " + "VALUES" + " ("
+                   + "ST_SetSRID(ST_Point(" + longitude + ", " + latitude + "),4326), "
+                   + reported_gross_floor_area +", "
+                   + energy_star_score + " ,"
+                   + yearbuilt + ", "
+                   + `'${postal_code}'` + ", "
+                   + `'${city}'` + ", "
+                   + site_eui + ", "
+                   + source_eui + ", "
+                   + latitude + ", "
+                   + longitude + ", "
+                   + `'${reported_address}'` + ", "
+                   + year + ", "
+                   + `'${property_name}'` + ", "
+                   + `'${property_type}'` + ", "
+                   + total_ghg_emissions_intensity + ", "
+                   + total_ghg_emissions + ", "
+                   + numunits + ", "
+                   + numfloors + ", "
+                   + numbuildings
+                   + ")&api_key=" + config.cartoApi
+            request.post( insertUrl, function(err, res, body) {
               if (err) {
                 console.log(err);
                 return err;
               }
-              console.log(body);
+              console.log("Upload Successful")
             }
           );
         });
